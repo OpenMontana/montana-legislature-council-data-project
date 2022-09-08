14563529c7b75b6f6ba46d63fee880a29fabb7d0
Smai Fullerton <shfullerton@gmail.com>
2022-09-07 18:51:52 -0600

    How the cookie crumbles

---
+++ python/cdp_montana_legislature_backend/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for [A
+CDP Instance backend.
+"""
+
+__author__ = "Open Montana"
+__version__ = "1.0.0"
+
+
+def get_module_version() -> str:
+    return __version__
---
+++ web/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-montana-legislature",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "[A",
+        timeZone: "America/Missoula",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
