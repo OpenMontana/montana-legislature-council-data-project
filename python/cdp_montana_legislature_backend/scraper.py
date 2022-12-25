9d320978ff6d6e164419a3d8704d326518a0defb
Smai Fullerton <shfullerton@gmail.com>
2022-12-25 15:34:18 -0700

    Format

--- python/cdp_montana_legislature_backend/scraper.py
+++ python/cdp_montana_legislature_backend/scraper.py
@@ -62,25 +62,27 @@ def get_events(
     key_bill_names = ["HB 6"]
     from_dt = datetime(2021, 4, 20)
     to_dt = datetime(2021, 4, 27)
-    
+
     # Start at the big table of all bills.
-    bills_url_2021 = "http://laws.leg.mt.gov/legprd/LAW0217W$BAIV.return_all_bills?P_SESS=20211"
+    bills_url_2021 = (
+        "http://laws.leg.mt.gov/legprd/LAW0217W$BAIV.return_all_bills?P_SESS=20211"
+    )
     # bills_url_2023 = "http://laws.leg.mt.gov/legprd/LAW0217W$BAIV.return_all_bills?P_SESS=20231"
     bills_html = requests.get(bills_url_2021).text
-    parsed_bills_html = BeautifulSoup(bills_html, 'html.parser')
-    bills_table = parsed_bills_html.find_all('table')[1]
+    parsed_bills_html = BeautifulSoup(bills_html, "html.parser")
+    bills_table = parsed_bills_html.find_all("table")[1]
     # Skip the first row because it's headings within a <tr>
-    bills_table_rows = bills_table.find_all('tr')[1:]
+    bills_table_rows = bills_table.find_all("tr")[1:]
     log.info(f"Found table with {len(bills_table_rows) - 1} rows.")
 
     # Store off the LAWS bill URLs for the bills of interest for the next step.
     bills_data = []
     for bill_row in bills_table_rows:
-        bill_link = bill_row.find_all('a')[0]
+        bill_link = bill_row.find_all("a")[0]
         bill_type_number = bill_link.text
         bill_data = {}
         if should_scrape_all_bills or bill_type_number in key_bill_names:
-            last_update_dt_str = (bill_row.find_all('td')[-2].text).split(';')[0]
+            last_update_dt_str = (bill_row.find_all("td")[-2].text).split(";")[0]
             last_update_date = datetime.strptime(last_update_dt_str, "%m/%d/%Y").date()
 
             # Only try to scrape this bill if it has been updated between the from_dt and to_dt params passed to the scraper.
@@ -88,74 +90,94 @@ def get_events(
             # is_bill_updated_after_specified_start = from_dt is None or last_update_date >= from_dt.date()
             # is_bill_updated_before_specified_end = to_dt is None or last_update_date <= to_dt.date()
             # if is_bill_updated_after_specified_start and is_bill_updated_before_specified_end:
-            bill_data['bill_type_number'] = bill_type_number
-            bill_data['description'] = bill_row.find_all('td')[-1].text
-            bill_data['laws_bill_url'] = "http://laws.leg.mt.gov/legprd/" + bill_link['href']
+            bill_data["bill_type_number"] = bill_type_number
+            bill_data["description"] = bill_row.find_all("td")[-1].text
+            bill_data["laws_bill_url"] = (
+                "http://laws.leg.mt.gov/legprd/" + bill_link["href"]
+            )
             bills_data.append(bill_data)
 
     event_data = []
     # Go to each LAWS bill URL and find bill actions that have associated recordings.
     for bill_data in bills_data:
         # log.info(f"Starting scrape of {bill_data}")
-        laws_bill_html = requests.get(bill_data['laws_bill_url']).text
+        laws_bill_html = requests.get(bill_data["laws_bill_url"]).text
         # We use regex search on the full html instead of going through BeautifulSoup due to "invalid" HTML returned by
         # the server that can't be parsed by BeautifulSoup.
-        bill_rows_with_recordings = re.findall('.*sliq.*', laws_bill_html)
+        bill_rows_with_recordings = re.findall(".*sliq.*", laws_bill_html)
         for bill_row in bill_rows_with_recordings:
-            parsed_bill_row = BeautifulSoup(bill_row, 'html.parser')
-            bill_cells = parsed_bill_row.find_all('td')
+            parsed_bill_row = BeautifulSoup(bill_row, "html.parser")
+            bill_cells = parsed_bill_row.find_all("td")
             hearing_date_str = bill_cells[1].text
             hearing_date = datetime.strptime(hearing_date_str, "%m/%d/%Y").date()
 
-            is_hearing_after_specified_start = from_dt is None or hearing_date >= from_dt.date()
-            is_hearing_before_specified_end = to_dt is None or hearing_date <= to_dt.date()
+            is_hearing_after_specified_start = (
+                from_dt is None or hearing_date >= from_dt.date()
+            )
+            is_hearing_before_specified_end = (
+                to_dt is None or hearing_date <= to_dt.date()
+            )
 
             if is_hearing_after_specified_start and is_hearing_before_specified_end:
-                sliq_links = bill_cells[-1].find_all('a', href=re.compile('sliq'))
+                sliq_links = bill_cells[-1].find_all("a", href=re.compile("sliq"))
 
                 hearing_data = {}
                 last_link_added = False
                 # Of the recordings available for this action, prefer using the video over the audio if video exists.
                 # If it doesn't exist, use the audio.
                 for link in sliq_links:
-                    sliq_link = link['href']
+                    sliq_link = link["href"]
                     sliq_html = requests.get(sliq_link).text
 
-                    media_info_regex = re.search('downloadMediaUrls = (.*);', sliq_html)
+                    media_info_regex = re.search("downloadMediaUrls = (.*);", sliq_html)
                     # No media is on the page as far as we are concerned
                     if media_info_regex is None:
                         continue
-                        
+
                     media_info = media_info_regex.groups()[0]
                     parsed_media_info = json.loads(media_info)[0]
-                    is_video = parsed_media_info['AudioOnly'] is False
+                    is_video = parsed_media_info["AudioOnly"] is False
 
                     if not last_link_added or is_video:
                         bill_action = bill_cells[0].text
-                        title = bill_data['bill_type_number'] + ' - ' + bill_action
+                        title = bill_data["bill_type_number"] + " - " + bill_action
                         committee = bill_cells[-1].text.strip()
-                        if not committee == '':
-                            title += ' - ' + committee
-                        hearing_data['title'] = title
-                        hearing_data['video_uri'] = parsed_media_info['Url']
+                        if not committee == "":
+                            title += " - " + committee
+                        hearing_data["title"] = title
+                        hearing_data["video_uri"] = parsed_media_info["Url"]
                         # The `external_source_id` will be used by the Capitol Tracker frontend to correlate the bill to
                         # the CDP event ID.
-                        hearing_data['external_source_id'] = sliq_link
+                        hearing_data["external_source_id"] = sliq_link
 
                         # Get the start and end time positions for the videos
-                        event_info_text = re.search('AgendaTree:(.*),', sliq_html).groups()[0]
+                        event_info_text = re.search(
+                            "AgendaTree:(.*),", sliq_html
+                        ).groups()[0]
                         event_info_json = json.loads(event_info_text)
                         parsed_url = urlparse(sliq_link)
                         # If there is no `agendaId` in the url, we are assuming timestamps haven't been added yet. For 10 more days,
                         # we will continue to try to scrape this video again until there are timestamps.
-                        if 'agendaId' in parse_qs(parsed_url.query):
-                            agenda_id = 'A' + parse_qs(parsed_url.query)['agendaId'][0]
-                            agenda_index = [i for i, d in enumerate(event_info_json) if agenda_id in d.values()][0]
-                            first_agenda_item_datetime_str = event_info_json[0]['startTime'].split('.', 1)[0]
-                            first_agenda_item_time = datetime.strptime(first_agenda_item_datetime_str, '%Y-%m-%dT%H:%M:%S').time()
-                            start_datetime_str = event_info_json[agenda_index]['startTime'].split('.', 1)[0]
-                            start_datetime = datetime.strptime(start_datetime_str, '%Y-%m-%dT%H:%M:%S')
-                            hearing_data['session_datetime'] = start_datetime
+                        if "agendaId" in parse_qs(parsed_url.query):
+                            agenda_id = "A" + parse_qs(parsed_url.query)["agendaId"][0]
+                            agenda_index = [
+                                i
+                                for i, d in enumerate(event_info_json)
+                                if agenda_id in d.values()
+                            ][0]
+                            first_agenda_item_datetime_str = event_info_json[0][
+                                "startTime"
+                            ].split(".", 1)[0]
+                            first_agenda_item_time = datetime.strptime(
+                                first_agenda_item_datetime_str, "%Y-%m-%dT%H:%M:%S"
+                            ).time()
+                            start_datetime_str = event_info_json[agenda_index][
+                                "startTime"
+                            ].split(".", 1)[0]
+                            start_datetime = datetime.strptime(
+                                start_datetime_str, "%Y-%m-%dT%H:%M:%S"
+                            )
+                            hearing_data["session_datetime"] = start_datetime
 
                             end_time = None
                             agenda_len = len(event_info_json)
@@ -167,41 +189,53 @@ def get_events(
                             # timestamp.
                             for i in range(1, agenda_len + 1):
                                 if agenda_len > agenda_index + i:
-                                    end_datetime_str = event_info_json[agenda_index + i]['startTime'].split('.', 1)[0]
-                                    end_time = datetime.strptime(end_datetime_str, '%Y-%m-%dT%H:%M:%S').time()
+                                    end_datetime_str = event_info_json[
+                                        agenda_index + i
+                                    ]["startTime"].split(".", 1)[0]
+                                    end_time = datetime.strptime(
+                                        end_datetime_str, "%Y-%m-%dT%H:%M:%S"
+                                    ).time()
 
                                 if end_time == start_datetime.time():
                                     continue
                                 else:
                                     break
 
-                            hearing_data['start_time'] = str(datetime.combine(date.min, start_datetime.time()) - datetime.combine(date.min, first_agenda_item_time))
+                            hearing_data["start_time"] = str(
+                                datetime.combine(date.min, start_datetime.time())
+                                - datetime.combine(date.min, first_agenda_item_time)
+                            )
                             if end_time is not None:
-                                hearing_data['end_time'] = str(datetime.combine(date.min, end_time) - datetime.combine(date.min, first_agenda_item_time))
+                                hearing_data["end_time"] = str(
+                                    datetime.combine(date.min, end_time)
+                                    - datetime.combine(date.min, first_agenda_item_time)
+                                )
 
                             last_link_added = True
 
                 event_data.append(hearing_data)
-    
+
     def create_ingestion_model(e):
         try:
             return EventIngestionModel(
-                body=Body(name=e['title']),
+                body=Body(name=e["title"]),
                 sessions=[
                     Session(
-                        video_uri=e['video_uri'],
-                        video_start_time=e['start_time'],
-                        video_end_time=e['end_time'],
-                        session_datetime=e['session_datetime'],
+                        video_uri=e["video_uri"],
+                        video_start_time=e["start_time"],
+                        video_end_time=e["end_time"],
+                        session_datetime=e["session_datetime"],
                         session_index=0,
-                        external_source_id=e['external_source_id']
+                        external_source_id=e["external_source_id"],
                     ),
                 ],
             )
         except Exception as exception:
-            log.info("===================================================\n\n\n" + 
-                "Got exception:\n\n {exception} \n\nFor this event:\n\n {e}\n\n\n" + 
-                "===================================================")
+            log.info(
+                "===================================================\n\n\n"
+                + "Got exception:\n\n {exception} \n\nFor this event:\n\n {e}\n\n\n"
+                + "==================================================="
+            )
 
     events = list(map(create_ingestion_model, event_data))
 
