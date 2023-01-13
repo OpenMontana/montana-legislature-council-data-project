d89437b4eb7f933389578b4210175c48d0a60f21
Wes Hargrove <wesleyhargrove@gmail.com>
2023-01-12 22:48:35 -0700

    lint

--- python/cdp_montana_legislature_backend/scraper_test.py
+++ python/cdp_montana_legislature_backend/scraper_test.py
@@ -2,8 +2,8 @@ from betamax.fixtures import unittest
 
 from cdp_montana_legislature_backend.scraper import LAWS_2023_ROOT_URL, get_bills
 
-class ScraperTestCase(unittest.BetamaxTestCase):
 
+class ScraperTestCase(unittest.BetamaxTestCase):
     def testGetBills(self):
         bills = get_bills(self.session, LAWS_2023_ROOT_URL)
         self.assertGreater(len(bills), 0)
