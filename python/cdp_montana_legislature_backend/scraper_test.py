c10dd03bf0c08cb1fbaa7148cc74668b4892dadd
Wes Hargrove <wesleyhargrove@gmail.com>
2023-01-12 22:11:43 -0700

    Add get_bills test

--- python/cdp_montana_legislature_backend/scraper_test.py
+++ python/cdp_montana_legislature_backend/scraper_test.py
@@ -1,13 +1,13 @@
 import unittest
 
+from cdp_montana_legislature_backend.scraper import LAWS_2023_ROOT_URL, get_bills
+
 class ScraperTestCase(unittest.TestCase):
 
-    @unittest.skip("fixme")
-    def test(self):
-        self.assertFalse(True)
-
-    def testSkip(self):
-        self.assertTrue(True)
+    # TODO use betamax to record sessions
+    def testGetBills(self):
+        bills = get_bills(LAWS_2023_ROOT_URL)
+        self.assertGreater(len(bills), 0)
 
 if __name__ == '__main__':
     unittest.main()
